59c907a2bbd7e0153906578a1767fe6e71cce00b
dong <115619547+omd0118@users.noreply.github.com>
2024-07-03 03:45:52 +0800

    Update 14. Longest Common Prefix.cpp

--- easy/14. Longest Common Prefix.cpp
+++ easy/14. Longest Common Prefix.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty()) return "";
         string a = strs[0];
-        for (string s : strs){
-            while(s.find(a) != 0){
-                a = a.substr(0,a.length()-1);
+        for(int i = 0; i < strs.size(); i++) {
+            string s = strs[i];
+            while(s.find(a) != 0){ //判斷a是不是s的前綴
+                a = a.substr(0,a.length() - 1);//每次扣除字串最後一個字
             }
         }
         return a;
